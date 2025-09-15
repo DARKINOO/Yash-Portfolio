caa960ec2e00c4a6540541782abf5efc8bb1ed67
Yash Jain <157146110+DARKINOO@users.noreply.github.com>
2025-09-15 14:15:59 +0530

    fixed Portfolio.jsx

--- Portfolio/src/components/Portfolio.jsx
+++ Portfolio/src/components/Portfolio.jsx
@@ -16,6 +16,7 @@ const projects = [
     links: {
       site: "https://parakhai-1.vercel.app/",
       github: "https://github.com/DARKINOO/Parakh.AI",
+    },
   },
   {
     img: hisab,
@@ -43,8 +44,8 @@ const Portfolio = () => {
     <div className='max-w-[1000px] mx-auto p-6 md:my-20' id='portfolio'>
       <h2 className='text-3xl font-bold dark:text-gray-200 mb-8'>Projects</h2>
       {projects.map((project, index) =>(
-        <Reveal>
-        <div key={index} className={`flex flex-col md:flex-row ${index % 2 !==0 ? 'md:flex-row-reverse' : ''} mb-12`}>
+        <Reveal key={index}>
+        <div className={`flex flex-col md:flex-row ${index % 2 !==0 ? 'md:flex-row-reverse' : ''} mb-12`}>
           <div className='w-full md:w-1/2 p-4'>
             <img src={project.img} alt={project.title}
              className='w-full h-full object-cover rounded-lg shadow-lg
